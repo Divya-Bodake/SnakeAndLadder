7ff786e90a20caeeffb3a8dd46ae5150d8ac9792
Divya-Bodake <divyabodake25@gmail.com>
2022-11-15 18:07:08 +0530

    [Divya]Add:Single Player at starting Position

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Snake snake = new Snake();
+            Console.WriteLine("Initial Position is {0}", snake.initialPosition);
+        }
+    }
+}
